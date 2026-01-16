7aed336e9eec160dafc8594db84f23ba98d47eac
MarcoRazafy <hei.marco.10@gmail.com>
2026-01-16 22:13:59 +0300

    ingredientSave2

--- src/main/java/com/TD2/Main.java
+++ src/main/java/com/TD2/Main.java
@@ -1,21 +1,25 @@
 package com.TD2;
+import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) {
+
         // Log before changes
         DataRetriever dataRetriever = new DataRetriever();
-        Dish dish = dataRetriever.findDishById(4
-
-        );
+        Dish dish = dataRetriever.findDishById(4);
         System.out.println(dish);
 
         // Log after changes
-//        dish.setIngredients(List.of(new Ingredient(1), new Ingredient(2)));
-//        Dish newDish = dataRetriever.saveDish(dish);
-//        System.out.println(newDish);
+        dish.setIngredients(List.of(new Ingredient(1), new Ingredient(2)));
+        Dish newDish = dataRetriever.saveDish(dish);
+        System.out.println(newDish);
 
         // Ingredient creations
-        //List<Ingredient> createdIngredients = dataRetriever.createIngredients(List.of(new Ingredient(null, "Fromage", CategoryEnum.DAIRY, 1200.0)));
-        //System.out.println(createdIngredients);
+        List<Ingredient> createdIngredients =
+                dataRetriever.createIngredients(
+                        List.of(new Ingredient(null, "Fromage", CategoryEnum.DAIRY, 1200.0))
+                );
+        System.out.println(createdIngredients);
     }
 }
